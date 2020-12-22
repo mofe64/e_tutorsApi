8ce95e77555a2823681960b2aa86e33d1067cea8
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2020-12-22 07:21:07 +0100

    project set up

---
+++ src/main/java/com/nubari/tutorsapi/RestOnlineTutorialApiApplication.java
@@ -0,0 +1,13 @@
+package com.nubari.tutorsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestOnlineTutorialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestOnlineTutorialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nubari/tutorsapi/RestOnlineTutorialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nubari.tutorsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestOnlineTutorialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
